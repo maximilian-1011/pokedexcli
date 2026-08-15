bc127806101f9878c12474a1d844948b42c88d3d
Maximilian <mbusch1011@gmail.com>
2026-08-15 12:50:31 +0200

    Refactored logic from main. Added "exit" and "help" commands

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-)
-
 func main()  {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		wordList := cleanInput(input)
-		fmt.Printf("Your command was: %s\n", wordList[0])
-	}
+	startRepl()
 }
